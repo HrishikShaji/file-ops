c8ae12873817ea1269dd29a56c452e561d8dd12a
HrishikShaji <hrishikminishaji@gmail.com>
2026-05-16 23:31:44 +0530

    added auth

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,23 @@
 // mod api;
-// mod async_api;
+mod async_api;
 // mod csv;
 // mod json;
 
-use reqwest::Error;
-use reqwest::header::USER_AGENT;
-use serde::Deserialize;
+use async_api::Result;
 
-#[derive(Deserialize, Debug)]
-struct User {
-    login: String,
-    id: u32,
-}
+use reqwest::Client;
+use reqwest::Error;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() -> Result<()> {
     // api::fetch_api();
     //async_api::fetch().await;
     //csv::read_csv_file();
     //json::create_json();
     //json::parse_json();
+    // async_api::fetch_github_stargazers().await?;
 
-    let request_url = format!(
-        "https://api.github.com/repos/{owner}/{repo}/stargazers",
-        owner = "rust-lang-nursery",
-        repo = "rust-cookbook"
-    );
+    async_api::auth_check().await?;
 
-    println!("{}", request_url);
-
-    let client = reqwest::Client::new();
-    let response = client
-        .get(&request_url)
-        .header(USER_AGENT, "rust-web-api-client-demo")
-        .send()
-        .await?;
-
-    let users: Vec<User> = response.json().await?;
-    println!("Users : {:?}", users);
     Ok(())
 }
