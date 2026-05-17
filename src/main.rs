a93d1a1c9041289688705646da9342c0189ac2f3
HrishikShaji <hrishikminishaji@gmail.com>
2026-05-17 12:57:24 +0530

    added download

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 // mod api;
-mod async_api;
+// mod async_api;
 // mod csv;
 // mod json;
 
-use async_api::Result;
+// use async_api::Result;
 
-use reqwest::Client;
-use reqwest::Error;
+// use reqwest::Client;
+// use reqwest::Error;
+
+use error_chain::error_chain;
+use std::fs::File;
+use std::io::copy;
+use tempfile::Builder;
+
+error_chain! {
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -16,8 +28,32 @@ async fn main() -> Result<()> {
     //json::create_json();
     //json::parse_json();
     // async_api::fetch_github_stargazers().await?;
+    // async_api::auth_check().await?;
 
-    async_api::auth_check().await?;
+    let temp_dir = Builder::new().prefix("example").tempdir()?;
+
+    let target = "https://www.rust-lang.org/logos/rust-logo-512x512.png";
+
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        println!("File name to download : {}", fname);
+        let fname = temp_dir.path().join(fname);
+        println!("will be located under: {:?}", fname);
+
+        File::create(fname)?
+    };
+
+    let content = response.text().await?;
+
+    copy(&mut content.as_bytes(), &mut dest)?;
 
     Ok(())
 }
